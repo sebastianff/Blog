dd567416c0a79d31ff396ed509410125dffeffe3
sebastianff <sebastianffarago@gmail.com>
2016-04-08 14:06:32 +0200

    feat: added error notifications

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -1,1 +1,5 @@
 Errors = new Mongo.Collection(null);
+
+throwError = function(message) {
+	Errors.insert({message: message});
+};
--- client/templates/posts/blogPosts.js
+++ client/templates/posts/blogPosts.js
@@ -5,3 +5,11 @@ Template.blogPosts.helpers({//here we populate the front page
     return Posts.find();
   }
 });
+
+Template.blogPosts.events({
+  'click glyphicon-thumbs-up': function (event, template) {
+  },
+  
+  'click glyphicon-thumbs-down': function (event, template) {
+  }
+});
