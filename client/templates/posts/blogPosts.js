c2e00bfde79e185a71c730bc2f553f29dbb181d9
sebastianff <sebastianffarago@gmail.com>
2016-04-08 12:12:04 +0200

    chore: cleaned up the delete function

--- client/templates/posts/blogPosts.js
+++ client/templates/posts/blogPosts.js
@@ -4,16 +4,4 @@ Template.blogPosts.helpers({//here we populate the front page
   posts:function(){
     return Posts.find();
   }
-})
-
-Template.blogPosts.events({//here we're not letting a person that didn't create the post delete it
-  "click .btn-lg": function (event, template) {
-    //if(Posts.findOne(event.target.name)==Meteor.userId())
-      //{
-        Posts.remove(event.target.name)
-      //}
-      ////else{
-      //alert("You are not authorized to delete that post")
-      //}
-	}
 });
